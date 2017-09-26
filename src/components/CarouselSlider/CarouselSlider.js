f1a2dff958004825b8cb3b095018195953432807
thinhnv789 <thinhnv.vnu@gmail.com>
2017-09-26 22:13:47 +0700

    slider

--- src/components/CarouselSlider/CarouselSlider.js
+++ src/components/CarouselSlider/CarouselSlider.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import Slider from 'react-slick'
+import ImageGallery from 'react-image-gallery';
 
+import 'react-image-gallery/styles/css/image-gallery.css';
 import './CarouselSlider.css'
 
 class CarouselSlider extends React.Component {
@@ -15,55 +16,38 @@ class CarouselSlider extends React.Component {
     // const { src } = this.props;
     let sources = [
       {
-        src: 'https://www.ific.ca/wp-content/uploads/2017/06/IFIC-Website-New-Investor-Centre-Carousel-Image.jpg',
-        alt: 'anhr 1'
+        original: 'http://skydoor.com.vn/images/slide/cua-luoi-chong-muoi-1.jpg',
+        thumbnail: 'http://skydoor.com.vn/images/slide/cua-luoi-chong-muoi-1.jpg',
+        originalAlt: 'Anh slide 1'
       },
       {
-        src: 'https://d1yupijb0jmhpf.cloudfront.net/mobilem8-php/wp-content/uploads/2017/08/AW_Carousel-ImSukJai-Desktop_1000x360Pixels_En_ol_opt.jpg',
-        alt: 'anhr 2'
+        original: 'http://skydoor.com.vn/images/slide/cua-luoi-chong-muoi-2.jpg',
+        thumbnail: 'http://skydoor.com.vn/images/slide/cua-luoi-chong-muoi-2.jpg',
+        originalAlt: 'Anh slide 2'
       },
       {
-        src: 'http://www.tireformula.com/assets/files/images/carousel/car-1.png',
-        alt: 'anhr 3'
-      },
-      {
-        src: 'http://www.musicvaultz.com/files/2016/12/CarouselUnit-1100.jpg',
-        alt: 'anhr 4'
-      },
-      {
-        src: 'http://mentorumc.org/images/uploads/carousel_images/Amazon_Smile_Carousel.jpg',
-        alt: 'anhr 5'
-      },
-      {
-        src: 'https://www.zigpress.com/wp-content/uploads/123rf-49462154-carousel-gear.jpg',
-        alt: 'anhr 6'
+        original: 'http://skydoor.com.vn/images/slide/cua-luoi-chong-muoi-3.jpg',
+        thumbnail: 'http://skydoor.com.vn/images/slide/cua-luoi-chong-muoi-3.jpg',
+        originalAlt: 'Anh slide 3'
       }
     ]
 
 
     const settings = {
-      autoplay: true,
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
+      autoPlay: true,
+      showThumbnails: false,
+      showBullets: true,
+      showFullscreenButton: false,
+      slideInterval: 2000,
       lazyLoad: true
     };
 
     return (
       <div className="slider">
-        <Slider {...settings}>
-          {
-            sources.map((item, index) => {
-              return (
-                <div key={index} className="slide-item">
-                  <img className="img img-responsive" src={item.src} alt={item.alt} style={{width: '100%'}}/>
-                </div>
-              )
-            })
-          }
-        </Slider>
+        <ImageGallery
+          items={sources}
+          {...settings}
+        />
       </div>
     )
   }
